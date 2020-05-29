694515fb03a19c355c9158f2a8b65f1fb729f7bf
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-29 16:23:05 -0500

    create Form component and join events

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
 import header from "../images/badge-header.svg";
 import "./styles/BadgeNew.css";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
   render() {
@@ -15,7 +16,7 @@ class BadgeNew extends React.Component {
 
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge
                 firstName="Richard"
                 lastName="Kaufman"
@@ -23,6 +24,9 @@ class BadgeNew extends React.Component {
                 jobTitle="Frontend Engineer"
                 avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
